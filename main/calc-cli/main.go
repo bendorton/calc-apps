6216d0011c2b27b6847228c4df0e49025b6f2632
Ben Dorton <ben.dorton@smarty.com>
2024-11-21 09:58:45 -0700

    Allow for '+', '-', '*', '/'

--- main/calc-cli/main.go
+++ main/calc-cli/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
-	"bendorton/calc-apps/handlers"
-
+	"github.com/bendorton/calc-apps/handlers"
 	"github.com/bendorton/calc-lib"
 )
 
 func main() {
-	handler := handlers.NewHandler(os.Stdout, &calc.Addition{})
+	var operation string
+	flag.StringVar(&operation, "op", "+", "The mathematical operation")
+	flag.Parse()
 
-	err := handler.Handle(os.Args[1:])
+	calculator, err := loadCalculator(operation)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	handler := handlers.NewHandler(os.Stdout, calculator)
+	err = handler.Handle(flag.Args())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func loadCalculator(operation string) (handlers.Calculator, error) {
+	switch operation {
+	case "+":
+		return &calc.Addition{}, nil
+	case "-":
+		return &calc.Subtraction{}, nil
+	case "*":
+		return &calc.Multiplication{}, nil
+	case "/":
+		return &calc.Division{}, nil
+	}
+	return nil, fmt.Errorf("invalid operation %q", operation)
 }
